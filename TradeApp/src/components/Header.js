42a41c52cce7e964de70234345255b91fc0fe0ac
Carlos BR <tolo130@hotmail.com>
2019-12-10 22:06:43 +0100

    Update Header as component

--- TradeApp/src/components/Header.js
+++ TradeApp/src/components/Header.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { Component } from "react";
 
 
-function Header() {
+export default class Header extends Component {
 
-    return (
+
+    render(){
+      return (
        
-      <header>
-        <nav>
-          <ul className="navItems">
-            <li className="navigation">
-              <a href="/">Home</a>
-            </li>
-            <li className="user-options">
-              <button type="button" class="badge badge-pill badge-warning">
-              Favourites <span class="badge badge-light"> 0</span>
-              </button>
-            </li>
-            <li className="user-options"> 
-              <a href="/AddItem">Add</a>
-            </li>
-            <li className="user-options"> 
-              <a href="/login">Login</a>
-            </li>
-            <li className="user-options">
-              <a href="/registration">Register</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+        <header>
+          <nav>
+            <ul className="navItems">
+              <li className="navigation">
+                <a href="/">Home</a>
+              </li>
+              <li className="user-options">
+                <button type="button" class="badge badge-pill badge-warning">
+                Favourites <span class="badge badge-light"> fav </span>
+                </button>
+              </li>
+              <li className="user-options"> 
+                <a href="/AddItem">Add</a>
+              </li>
+              <li className="user-options"> 
+                <a href="/login">Login</a>
+              </li>
+              <li className="user-options">
+                <a href="/registration">Register</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+  
+      );
+     
 
-    );
-   
+    }
+
   }
   
-  export default Header;
